08997c7a389ceef6f5c557f57b1a23d40a0fde37
ronaldconn <97855104+ronaldconn@users.noreply.github.com>
2022-09-06 19:54:23 -0700

    server js update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const todoRoutes = require('./routes/todos')
 const PORT = process.env.PORT || PORT
 
 // Load config by calling dotenv and creating an object with the path
-require('dotenv').config({ path: './.env' })
+require('dotenv').config({ path: './config/.env' })
 
 // Passport config
 require('./config/passport')(passport)
